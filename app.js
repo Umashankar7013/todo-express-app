fab74128d970812c65b097a65c395700520b3848
Uma shankar <uma.shankar@in.geekyants.com>
2024-04-11 18:41:50 +0530

    change

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello Tilicho");
-});
-
 const { router } = require("./src/routes/index");
 
 app.use("/", router);
+
+app.get("/", (req, res) => {
+  res.send("Hello Tilicho");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
